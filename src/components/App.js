2dd4db619bfb3e0cbd5a26402fc701d9b4da6e20
khuong <tranvinhkhuong@gmail.com>
2020-08-03 17:14:30 -0700

    update

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import UserCreate from './UserCreate'
+
+const App = () => {
+    const [language, setLanguage] = useState('english')
+    return (
+        <div className='ui container'>
+            <div>
+                Select a language:
+                <i
+                    className='flag us'
+                    onClick={() => setLanguage('english')} />
+                <i
+                    className='flag nl'
+                    onClick={() => setLanguage('dutch')} />
+            </div>
+            <UserCreate />
+        </div>
+    )
+}
+
+export default App
